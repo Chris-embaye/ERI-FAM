3bcd1481d14b8fd231b223caf41d01852fee5e5a
Chris Embaye <mebrahatom12@gmail.com>
2026-05-01 00:01:54 -0500

    fix: add firebase-config.js to admin folder and fix script path for hosting

---
+++ admin/firebase-config.js
@@ -0,0 +1,21 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyCcvA7TBrkdsfXVSIT-J9U-asNsIKWvX2E",
+  authDomain:        "eri-fam.firebaseapp.com",
+  databaseURL:       "https://eri-fam-default-rtdb.firebaseio.com",
+  projectId:         "eri-fam",
+  storageBucket:     "eri-fam.firebasestorage.app",
+  messagingSenderId: "640644486226",
+  appId:             "1:640644486226:web:9a076e9775c58763cffb8b",
+  measurementId:     "G-GMYL5P3F1P"
+};
+
+const ADMIN_EMAIL = "embayechris@gmail.com";
+
+const AUDD_API_KEY   = "";
+const GEMINI_API_KEY = "";
+
+const CLOUDINARY_CLOUD  = "dcbqqqpmw";
+const CLOUDINARY_PRESET = "eri-fam-music";
+
+const YOUTUBE_API_KEY = "";
+const YT_BACKEND_URL  = "";
